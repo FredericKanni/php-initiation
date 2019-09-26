994be42480adcd751eeaa0dd672c48fa7c1b2888
frederic <kannif@ymail.com>
2019-09-26 14:02:12 +0400

    concatenation et modulo

--- index.php
+++ index.php
@@ -27,3 +27,50 @@ echo $age_du_visiteur;
 $poids = 57.3;
 echo $poids;
 ?>
+
+<?php
+echo "concatenation";
+echo "<br>";
+$age_du_visiteur = 17;
+echo "Le visiteur a ";
+echo $age_du_visiteur;
+echo " ans";
+?>
+
+
+
+<?php
+$nombre = 2 + 4; // $nombre prend la valeur 6
+echo $nombre;
+echo "<br>";
+$nombre = 5 - 1; // $nombre prend la valeur 4
+echo $nombre;
+echo "<br>";
+$nombre = 3 * 5; // $nombre prend la valeur 15
+echo $nombre;
+echo "<br>";
+$nombre = 10 / 2; // $nombre prend la valeur 5
+echo $nombre;
+echo "<br>";
+
+// Allez on rajoute un peu de difficulté
+$nombre = 3 * 5 + 1; // $nombre prend la valeur 16
+echo $nombre;
+echo "<br>";
+$nombre = (1 + 2) * 2; // $nombre prend la valeur 6
+echo $nombre;
+echo "<br>";
+?>
+
+
+<?php
+echo "modulo";
+echo "<br>";
+
+$nombre = 10 % 5; // $nombre prend la valeur 0 car la division tombe juste
+echo $nombre;
+echo "<br>";
+$nombre = 10 % 3; // $nombre prend la valeur 1 car il reste 1
+echo $nombre;
+echo "<br>";
+?>
