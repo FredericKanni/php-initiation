54b9636609385467d5c2688f09eba8dbbeaaa71e
frederic <kannif@ymail.com>
2019-09-26 13:53:57 +0400

    les variables

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$age_du_visiteur = 17; // La variable est créée et vaut 17
+$age_du_visiteur = 23; // La variable est modifiée et vaut 23
+$age_du_visiteur = 55; // La variable est modifiée et vaut 55
+echo $age_du_visiteur;
+?>
+
+
+<?php
+$nom_du_visiteur = "Mateo21";
+$nom_du_visiteur = 'Mateo21';
+echo $nom_du_visiteur;
+?>
+
+<?php
+$variable = 'Mon "nom" est Mateo21';
+$variable = "Je m'appelle Mateo21";
+echo $variable;
+?>
+
+<?php
+$age_du_visiteur = 17;
+echo $age_du_visiteur;
+?>
+
+<?php
+$poids = 57.3;
+echo $poids;
+?>
