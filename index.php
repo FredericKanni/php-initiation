4ea4122e45f9a8b2eb3f62e8d45533c490f878bc
frederic <kannif@ymail.com>
2019-09-26 15:13:28 +0400

    condition

--- index.php
+++ index.php
@@ -74,3 +74,66 @@ $nombre = 10 % 3; // $nombre prend la valeur 1 car il reste 1
 echo $nombre;
 echo "<br>";
 ?>
+
+
+
+<?php
+echo "La structure de base : if… else";
+echo "<br>";
+?>
+
+
+<?php
+$age = 8;
+ 
+if ($age <= 12) // SI l'âge est inférieur ou égal à 12
+{
+    echo "Salut gamin ! Bienvenue sur mon site !<br />";
+    $autorisation_entrer = "Oui";
+}
+else // SINON
+{
+    echo "Ceci est un site pour enfants, vous êtes trop vieux pour pouvoir  entrer. Au revoir !<br />";
+    $autorisation_entrer = "Non";
+}
+echo "Avez-vous l'autorisation d'entrer ? La réponse est :  $autorisation_entrer ";
+echo "<br>";
+echo "Avez-vous l'autorisation d'entrer ? La réponse est : " . $autorisation_entrer ;
+echo "<br>";
+echo 'Avez-vous l\'autorisation d\'entrer ? La réponse est : ' . $autorisation_entrer ;
+?>
+
+
+
+<?php
+
+echo "<br>";
+//$autorisation_entrer = "Non";
+
+if ($autorisation_entrer == "Oui") // SI on a l'autorisation d'entrer
+{
+    // instructions à exécuter quand on est autorisé à entrer
+    echo "Euh, je  connais  ton âge, tu peux me le rappeler s'il te plaît ?";
+}
+elseif ($autorisation_entrer == "Non") // SINON SI on n'a pas l'autorisation d'entrer
+{
+    // instructions à exécuter quand on n'est pas autorisé à entrer
+}
+else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
+{
+    echo "Euh, je ne connais pas ton âge, tu peux me le rappeler s'il te plaît ?";
+}
+?>
+
+<?php
+ static $autorisation_entrer = true;
+
+if ($autorisation_entrer)
+{
+    echo "Bienvenue petit nouveau. :o)";
+}
+else
+{
+    echo "T'as pas le droit d'entrer !";
+}
+?>
